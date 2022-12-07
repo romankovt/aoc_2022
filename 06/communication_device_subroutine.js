8d493dd320208cfb8d4beedc318d6bbc2b16f91f
Roman Kovtunenko <roman.kovtunenko@gmail.com>
2022-12-07 03:12:11 +0200

    Solve day 6 of advent of code

---
+++ 06/communication_device_subroutine.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+let input = fs
+  .readFileSync(process.cwd() + "/input.txt")
+  .toString()
+  .trim()
+
+let slidingArray = []
+
+for (let i = 4; i < input.length; i++)  {
+  slidingArray = input.slice(i - 4, i).split("");
+
+  if (slidingArray.every((char, index) => slidingArray.indexOf(char) == index)) {
+    console.log(slidingArray);
+    console.log(`We need to process ${i} chars before message starts`)
+    break;
+  }
+}
+
+console.time("second loop took");
+for (let i = 14; i < input.length; i++)  {
+  slidingArray = input.substring(i - 14, i).split("");
+
+  if ((new Set(slidingArray)).size == slidingArray.length) {
+    console.log(`We need to process ${i} chars before message starts`)
+    break;
+  }
+}
+console.timeEnd("second loop took");
